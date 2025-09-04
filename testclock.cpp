4611958c5257d74a1b16b070f7488f1b8bfc906a
James Skön <skonjp@kenyon.edu>
2025-09-04 14:15:39 -0400

    Added newlines to test program

--- testclock.cpp
+++ testclock.cpp
@@ -11,32 +11,38 @@ int main() {
     clockType clock2(12, 30, 45);
     std::cout << "Initial time (clock2, parameterized constructor): ";
     clock2.printTime();  // Expected: 12:30:45
+    std::cout << std::endl;
 
     // Test setTime function
     clock1.setTime(23, 59, 58);
     std::cout << "Time after setting (clock1): ";
     clock1.printTime();  // Expected: 23:59:58
+    std::cout << std::endl;
 
     // Test incrementSeconds function
     clock1.incrementSeconds();
     std::cout << "Time after incrementing seconds (clock1): ";
     clock1.printTime();  // Expected: 23:59:59
+    std::cout << std::endl;
 
     clock1.incrementSeconds();
     std::cout << "Time after another increment (clock1): ";
     clock1.printTime();  // Expected: 00:00:00
+    std::cout << std::endl;
 
     // Test incrementMinutes function
     clock1.setTime(23, 59, 58);
     clock1.incrementMinutes();
     std::cout << "Time after incrementing minutes (clock1): ";
     clock1.printTime();  // Expected: 00:00:58
+    std::cout << std::endl;
 
     // Test incrementHours function
     clock1.setTime(23, 59, 58);
     clock1.incrementHours();
     std::cout << "Time after incrementing hours (clock1): ";
     clock1.printTime();  // Expected: 00:59:58
+    std::cout << std::endl;
 
     // Test getTime function
     int hours, minutes, seconds;
